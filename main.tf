8730ae5d0f133c253d2cf02b1e8dbb89ea6ad5e6
ptonini <pedro.tonini@hotmail.com>
2023-05-31 18:38:29 -0300

    fix: added tags field to ignore_changes

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "azurerm_private_dns_zone" "this" {
   provider            = azurerm
   name                = var.name
   resource_group_name = var.rg.name
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
@@ -11,4 +16,9 @@ resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   private_dns_zone_name = azurerm_private_dns_zone.this.name
   virtual_network_id    = each.value.id
   resource_group_name   = var.rg.name
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
 }
